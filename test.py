68ccd1d4a8b5ed6b85dd7a18b73575362239e93b
Xinyi Ji <jxy@LAPTOP-E617K8EF.localdomain>
2019-01-19 14:41:12 -0500

    now the function can generate all the information in the list

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ from google.cloud.language import types
 client = language.LanguageServiceClient()
 
 # The text to analyze
-text = u'Jenny love Trump. She think he is so handsome'
+text = u'Jenny love Trump. She think he is so handsome. She also like ice-cream'
 document = types.Document(
     content=text,
     type=enums.Document.Type.PLAIN_TEXT)
@@ -15,8 +15,23 @@ document = types.Document(
 # Detects the sentiment of the text
 sentiment = client.analyze_sentiment(document=document).document_sentiment
 # Detects the entities of the text
-entities = client.analyze_entity_sentiment(document=document, encoding_type='UTF32')
+response = client.analyze_entities(document=document, encoding_type='UTF32')
+i=0
+text_name=[]
+text_type=[]
+text_metadata=[]
+text_salience=[]
+
+for entity in response.entities:
+    text_name.append(entity.name)
+    text_type.append(entity.type)
+    text_metadata.append(entity.metadata)
+    text_salience.append(entity.salience)
+    i=i+1
+
+
+
 
 print('Text: {}'.format(text))
 print('Sentiment: {}, {}'.format(sentiment.score, sentiment.magnitude))
-print(entities.entities[0].name)
+print(text_name)
